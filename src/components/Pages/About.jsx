e837a77b3743131c0c64c921a9509f17265f7b67
Mayuresh Marathe <mayuresh.marathe123@gmail.com>
2024-07-13 19:40:30 +0530

    Changed the website name and changed the hoem page and about content.

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -12,16 +12,17 @@ export default function About() {
                     </div>
                     <div className="md:7/12 lg:w-6/12">
                         <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
-                            React development is carried out by passionate developers
+                            About Me
                         </h2>
                         <p className="mt-6 text-gray-600">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
-                            accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
-                            aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
+                            I am a primeraly Java and Spring Boot developer who has worked on a 
+                            Spring Boot Microservices project for a mobile banking app. I have worked on this project for past 2 years
+                            and have gain in depth knoledge of developing a production grade Spring Boot Application. 
                         </p>
                         <p className="mt-4 text-gray-600">
-                            Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
-                            Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
+                            And apart from Java and Spring on my own I have also learnt the kubernetes and React. I like to learn 
+                            and implement new things. For this simple website also I have used React as fontend and appwrite as a 
+                            backend as a service.
                         </p>
                     </div>
                 </div>
--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -38,7 +38,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    Acme Inc, Street, State, Postal Code
+                                    Nishigandha Society, Mahalaxminagar, Nere, Panvel - 410206
                                 </div>
                             </div>
 
@@ -60,7 +60,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    +44 1234567890
+                                    +91 9561219909
                                 </div>
                             </div>
 
@@ -82,7 +82,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    info@acme.org
+                                    mayuresh.marathe123@gmail.com
                                 </div>
                             </div>
                         </div>
